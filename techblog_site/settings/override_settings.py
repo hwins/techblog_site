f4c798106b4690942a1d6537ef2a54ca4473ed59
hwins <info@howardwinston.com>
2014-03-31 13:42:35 -0400

    css add margin-bottom more link class

--- techblog_site/settings/override_settings.py
+++ techblog_site/settings/override_settings.py
@@ -1,25 +1,26 @@
 # this module will override default settings with
 # development environment values
 
-# import os
-# #
-# path = os.path.abspath(os.path.dirname(__file__))
-# PROJECT_PATH = os.path.normpath(os.path.join(path,  ".."))
-# #
-# DEBUG = True
-# TEMPLATE_DEBUG = True
-# #
-# MEDIA_ROOT = os.path.join(PROJECT_PATH, '../media/')
-# MEDIA_URL = '/media/'
+import os
+# # #
+path = os.path.abspath(os.path.dirname(__file__))
+PROJECT_PATH = os.path.normpath(os.path.join(path,  ".."))
+# # #
+DEBUG = True
+TEMPLATE_DEBUG = True
+# # # #
+# # MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media/')
+# # MEDIA_URL = '/media/'
+# #  
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static/'),
+)
+#  
+ENDLESS_PAGINATION_PER_PAGE = 2
 # 
-# STATIC_ROOT = ''
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_PATH, 'static/'),
-# )
-# 
-# ENDLESS_PAGINATION_PER_PAGE = 2
-# 
-# HOLD_TWITTERJS = True
+HOLD_TWITTERJS = True
 MEDIA_ROOT = '/home/developer/techblog_site/media/'
 MEDIA_URL = '/media/'
+# ALLOWED_HOSTS = ['127.0.0.1']
